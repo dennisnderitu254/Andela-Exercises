57a68aa987b845c238074c5f20d3eb893013c536
Dennis Nderitu <dknderitu@gmail.com>
2017-10-01 23:49:05 +0300

    Using the eval function in basic arithmetic

---
+++ addintegers4.py
@@ -0,0 +1,5 @@
+print("Add a comma between the digits")
+# The eval function dynamically translates the text provided by the user into an executable form that the
+# program can process
+num1, num2 = eval(input('Please enter number 1, number 2:'))
+print(num1, '+', num2, '=', num1 + num2)
