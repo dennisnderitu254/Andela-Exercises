9c83f6e2f015aa04233a91f8b66d4546b720dd1b
Dennis Nderitu <dknderitu@gmail.com>
2017-10-01 23:47:02 +0300

    Implementing functional composition

---
+++ addintegers3.py
@@ -0,0 +1,4 @@
+# Exhibiting functional composition
+num1 = int(raw_input('Please enter an integer value:'))
+num2 = int(raw_input('Please enter another integer value:'))
+print(num1, '+', num2, '=', num1 + num2)
