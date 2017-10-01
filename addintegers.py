0673dc2704cba8be147d67e08b8d51facdb68521
Dennis Nderitu <dknderitu@gmail.com>
2017-10-01 23:44:11 +0300

    adding integers

---
+++ addintegers.py
@@ -0,0 +1,8 @@
+print('Please enter an integer value:')
+x = raw_input()
+print('please enter another integer value:')
+y = raw_input()
+num1 = int(x)
+num2 = int(y)
+
+print(num1, 'x', num2, '=', num1+num2)
