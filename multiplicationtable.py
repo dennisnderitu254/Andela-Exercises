602b8359b494848d3cfa1030e05bcf890edadd8d
Dennis Nderitu <dknderitu@gmail.com>
2017-10-04 12:51:30 +0300

    multiplication tables first version

---
+++ multiplicationtable.py
@@ -0,0 +1,21 @@
+# Nested for loop Example
+
+# Print a multiplication table to 10 * 10
+# Print column heading
+
+print("         1   2   3   4   5   6   7   8   9   10")
+print("     +---------------------------------------------")
+
+for row in range(1, 11):
+	if row < 10:
+		print(" ", end=" ")
+	print(row, "| ", end=" ")
+
+	for column in range(1, 11):
+		product = row*column;
+		if product < 100:
+			print(end=" ")
+		if product < 10:
+			print(end=" ")
+		print(product, end=" ")
+	print()
