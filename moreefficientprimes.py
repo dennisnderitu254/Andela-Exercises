d5dbc67654e3beb312e447f3354c4f669c5ce393
Dennis Nderitu <dknderitu@gmail.com>
2017-10-10 23:57:54 +0300

    getting prime numbers

---
+++ moreefficientprimes.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+max_value  = eval(input('Display primes up to what value?'))
+value = 2 # smallest prime number
+
+while value <= max_value:
+	# See if value is prime
+	is_prime = True # Provisionally, value is prime
+	# Try all possible factors from 2 to value -1
+	trial_factor = 2
+	root = sqrt(value)
+	while trial_factor <= root:
+		if value % trial_factor == 0:
+			is_prime = False; # Found a factor
+			break             # No need to continue; it is NOT prime
+		trial_factor += 1     # Try the next potential factor
+	if is_prime:
+		print(value, end= ' ') # Display the prime number
+	value += 1                 # Try the next potential prime number
+
+print() # Move cursor down to next  line
