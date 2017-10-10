67db6e0c09923b3e83ff451918fa6648df06c104
Dennis Nderitu <dknderitu@gmail.com>
2017-10-10 23:51:33 +0300

    finding squareroots

---
+++ standardsquareroot.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+#Get value from the user
+num = eval(input("Enter number:"))
+
+#Compute the square root
+root = sqrt(num);
+
+# Report result
+print("Square root of", num, "=", root)
+
+sqrt(num)
