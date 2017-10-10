920a6f9d3a47c0f796f93db45ceae02e7fc61ba5
Dennis Nderitu <dknderitu@gmail.com>
2017-10-10 23:37:00 +0300

    probability die

---
+++ die.py
@@ -0,0 +1,35 @@
+from random import randrange
+# Roll the die three times
+for i in range(0, 3):
+	# Generate random number in the range 1...6
+	value = randrange(1,6)
+
+	# Show the die
+	print("+-------+")
+	if value == 1:
+		print("|     |")
+		print("|  *  |")
+		print("|     |")
+	elif value == 2:
+		print("| *   |")
+		print("|     |")
+		print("|   * |")
+	elif value == 3:
+		print("|     *|")
+		print("|   *  |")
+		print("| *    |")
+	elif value == 4:
+		print("| *   *|")
+		print("|      |")
+		print("| *   *|")
+	elif value == 5:
+		print("| *    *|")
+		print("|   *   |")
+		print("| *    *|")
+	elif value == 6:
+		print("| * * * |")
+		print("|       |")
+		print("| * * * |")
+	else:
+		print(" *** Error: illegal die value ***")
+	print("+--------+")
