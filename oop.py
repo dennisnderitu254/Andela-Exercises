f1f16de1465f13999010fdbf9201aeae2e6dcb5f
Dennis Nderitu <dknderitu@gmail.com>
2017-10-11 12:33:53 +0300

    using classes

---
+++ oop.py
@@ -0,0 +1,10 @@
+""" complete the code to allow CRUD operations """
+
+class User(object):
+	# docstring for User
+	def __init__(self, arg):
+		super(User, self).__init__()
+		self.arg = arg
+
+
+		
