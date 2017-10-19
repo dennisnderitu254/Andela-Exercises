71f990871f7868038ca12145978949537fa39ac3
dennisnderitu254 <dknderitu@gmail.com>
2017-10-19 22:51:38 +0300

    Program that draws trees of various heights

--- treefunc.py
+++ treefunc.py
@@ -8,10 +8,10 @@ def tree(height):
 		# Print leading spaces
 		count = 0
 		while count < height - row:
-			print(end=" ")
+			print(end =" ")
 			count += 1
 			# Print out stars, twice the current row plus one:
-			#1. number of stars on left side of tree
+			# 1. number of stars on left side of tree
 			# = current row value
 			# 2. exactly one star in the center of tree
 			# 3. number of stars on right side of tree
