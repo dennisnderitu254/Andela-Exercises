f2fedd4e73b0201a6e7cfd3e14701653e176b64a
dennisnderitu254 <dknderitu@gmail.com>
2017-10-31 16:18:07 +0300

    finding the majic number

---
+++ 10_python.py
@@ -0,0 +1,15 @@
+magicNumber = 26
+
+# ok this program find a magic number
+
+'''
+multiple comment
+print("Dennis" + "Nderitu")
+'''
+
+for n in range(101):
+    if n is magicNumber:
+        print(n, "is the magic number ! ")
+        break
+    else:
+        print(n)
